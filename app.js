5e522c10cba87cc83c47409887e11615d374ef68
Muaaz Ahmed Baig <muaazahmed0111@gmail.com>
2024-12-07 16:07:28 +0500

    A-JS Assignment 4

---
+++ app.js
@@ -0,0 +1,49 @@
+const arrays = [ 42, true, "hello world", {name: "John", age:22, isStudent: false,}, false, 3.14, "JavaScript is fun"];
+
+
+// Q1
+// let mapMethod = arrays.map((e) =>{
+//     let mystring = String(e);
+//     console.log(mystring);
+// });
+
+// Q2 
+// let filterMethod = arrays.filter(element => 
+//   typeof element === "number"
+// );
+// console.log(filterMethod);
+
+// Q3
+// let forEachMethod = arrays.forEach(element => 
+//     console.log(typeof element)
+// );
+
+// Q4
+// let reduceMethod = arrays.reduce((sum, element) => {
+//     return typeof element === "number" ? sum + element : sum;
+//   }, 0);
+//   console.log(reduceMethod);
+
+// Q5
+// let findMethod = arrays.find(element => 
+//     typeof element === "boolean"
+// );
+// console.log(findMethod);
+
+// Q6
+// let findIndexMethod = arrays.findIndex(element => 
+//     typeof element === "object"
+// );
+// console.log(findIndexMethod);
+
+// Q7
+// let someMethod = arrays.some(element => 
+//     typeof element === "number"
+// );
+// console.log(someMethod);
+
+// Q8
+// let everyMethod = arrays.every(element => 
+//     typeof element === "string"
+// );
+// console.log(everyMethod);
